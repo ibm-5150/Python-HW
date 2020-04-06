7558b9f5c13a9f716525472e3535b3f3695abfbe
ibm-5150 <59841876+ibm-5150@users.noreply.github.com>
2020-04-06 16:11:52 -0700

    Create task_1.py

---
+++ HW-5/task_1.py
@@ -0,0 +1,11 @@
+#\ / : * ? " < > |
+
+try:
+	file_name = input()
+	file = open(file_name, "w")
+	file.close()
+except PermissionError:
+	print("A file name can't contain any of the following characters:" + '\n\\ / : * ? " < > |')
+except OSError:
+	print("A file name can't contain any of the following characters: " + '\n\\ / : * ? " < > |')
+
