dc8b73bebef8e0d683c0b6b338e1a4bffde98441
ibm-5150 <59841876+ibm-5150@users.noreply.github.com>
2020-04-07 06:20:50 -0700

    Add files via upload

---
+++ HW-5/task_5.py
@@ -0,0 +1,27 @@
+#-----------------------------------------------------------
+# SyntaxError          #while True;
+	                   #print(1)
+#-----------------------------------------------------------
+# IndentationError     #while True:
+		          	   #print(1)
+#-----------------------------------------------------------
+# NameError            #prin('hello')
+#-----------------------------------------------------------
+# IndexError           #new_list = [1,2,3]
+			           #print(new_list[3])
+#-----------------------------------------------------------
+# KeyError             #new_dict = {'key_1' : 1,'key_2' : 2}
+		               #print(new_dict['key_3'])
+#-----------------------------------------------------------
+# ImportError          #import randomn
+#-----------------------------------------------------------
+# AttributeError       #a = 8
+                       #print(a.append(4))
+#-----------------------------------------------------------
+# ZeroDivisionError    #print(1/0)
+#-----------------------------------------------------------
+# RuntimeError         #A run-time error happens when Python 
+                       #understands what you are saying, but 
+                       #runs into trouble when following your
+                       #instructions
+#-----------------------------------------------------------
