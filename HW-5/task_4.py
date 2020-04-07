75a985273b83cb2dd55b5cc0ab90e11674caf75f
ibm-5150 <59841876+ibm-5150@users.noreply.github.com>
2020-04-07 07:47:40 -0700

    Add files via upload

---
+++ HW-5/task_4.py
@@ -0,0 +1,18 @@
+k = int(input("Enter amount of characters to delete in file: "))
+file_in = open('task_4.txt','r')
+file_out = open('task_4_out.txt','w')
+lines = file_in.readlines()
+
+line_symbols = list()
+for x in lines:
+	line_symbols.append(x[k:])
+
+file_string = ''
+for element in line_symbols:
+	file_string = file_string + element
+
+file_out.write(file_string)
+
+file_in.close()
+file_out.close()
+
